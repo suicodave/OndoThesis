661e1ad0e291b41f338894a3ecd4273706744bc2
Always Hungry <suico.dave@gmail.com>
2018-03-03 14:57:18 +0800

    'fix'

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $url = parse_url(getenv("JAWSDB_URL"));
-$host = '	g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$host = 'g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 $username = 'ra9crw5ngtoqh5it';
 $password = 'rqn336n6a781keyx';
 $database = 'vyo6kqeav67znjtr';
